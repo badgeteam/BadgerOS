21c3f995f2b3c4ca352c515229a1c36e2c196a82
Julian Scheffers <julian@scheffers.net>
2025-05-27 19:45:44 +0200

    Update thread bindings to use errno

--- kernel/src/bindings/thread.rs
+++ kernel/src/bindings/thread.rs
@@ -5,7 +5,10 @@ use core::{
 
 use alloc::{boxed::Box, ffi::CString};
 
-use super::raw::{self, SCHED_PRIO_NORMAL, badge_err_t, tid_t, timestamp_us_t};
+use super::{
+    error::ErrnoError,
+    raw::{self, SCHED_PRIO_NORMAL, tid_t, timestamp_us_t},
+};
 
 pub struct Thread {
     handle: tid_t,
@@ -22,24 +25,25 @@ impl Thread {
     pub fn try_new<T: FnOnce() -> i32 + Send + 'static>(
         code: T,
         name: Option<&str>,
-    ) -> Option<Self> {
+    ) -> Result<Self, ErrnoError> {
         unsafe {
             let closure: Box<dyn FnOnce() -> i32> = Box::new(code);
             let arg = Box::into_raw(Box::new(closure));
             let name = name.map(|f| CString::from_str(f).unwrap());
             let tid = raw::thread_new_kernel(
-                0 as *mut badge_err_t,
                 name.map(|f| f.as_ptr()).unwrap_or(0 as *const c_char),
                 Some(rust_thread_trampoline),
                 arg as *mut c_void,
                 SCHED_PRIO_NORMAL as c_int,
             );
-            if tid == 0 {
+            if tid <= 0 {
                 drop(Box::from_raw(arg));
-                None
+                Err(ErrnoError {
+                    errno: core::mem::transmute(-tid),
+                })
             } else {
-                raw::thread_resume(0 as *mut badge_err_t, tid);
-                Some(Self { handle: tid })
+                raw::thread_resume(tid);
+                Ok(Self { handle: tid })
             }
         }
     }
@@ -50,7 +54,7 @@ impl Thread {
         unsafe { raw::thread_join(self.handle) as i32 }
     }
     pub fn detach(self) {
-        unsafe { raw::thread_detach(0 as *mut badge_err_t, self.handle) }
+        unsafe { raw::thread_detach(self.handle) };
     }
 }
 
