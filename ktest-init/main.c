00136df556fec7240832f996e115f11523996f56
Julian Scheffers <julian@scheffers.net>
2026-03-17 13:50:23 +0100

    Signals testing

--- ktest-init/main.c
+++ ktest-init/main.c
@@ -3,36 +3,94 @@
 
 #include <dirent.h>
 #include <sys/statvfs.h>
-#include <inttypes.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/syscall.h>
 
-void run(char **argv) {
-    if (fork() == 0) {
-        if (execvp(argv[0], argv)) {
+void handler(int signo, siginfo_t *info, void *uctx) {
+    printf("Handled signal %d\n", signo);
+}
+
+void run(char const *const *argv) {
+    for (size_t i = 0; argv[i]; i++) {
+        printf("argv[%zu] = %s\n", i, argv[i]);
+    }
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (execvp(argv[0], (char**)argv)) {
             perror("execvp");
+        }
+    } else {
+        int wstatus;
+        if (waitpid(pid, &wstatus, 0)) {
+            if (WIFEXITED(wstatus)) {
+                printf("Exit %d\n", WEXITSTATUS(wstatus));
+            } else if (WIFSIGNALED(wstatus)) {
+                printf("Signal %d\n", WTERMSIG(wstatus));
+            }
         }
     }
 }
 
-int main(int argc, char **argv, char **envp) {
-    printf("Hello, mlibc!\n");
-
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, argv[i]);
+void readline(char *buffer, size_t const buffer_len) {
+    for (size_t i = 0; i < buffer_len; i++) {
+        int c;
+        do {
+            c = fgetc(stdin);
+        } while(c == EOF);
+        buffer[i] = c;
+        if (c == '\r' || c == '\n') {
+            printf("\n");
+            break;
+        }
+        fputc(c, stdout);
+        fflush(stdout);
     }
+}
 
-    while (*envp) {
-        printf("env: %s\n", *envp);
-        envp++;
+int main(int argc, char **argv, char **envp) {
+    setenv("PATH", "/usr/bin:/usr/sbin", 1);
+    
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_sigaction = handler;
+    if (sigaction(60, &sa, NULL)) {
+        perror("sigaction");
     }
     
-    DIR *dirp = opendir("/");
-    struct dirent *dent;
-    while ((dent = readdir(dirp))) {
-        printf("%s type %02x ino %" PRId64 "\n", dent->d_name, dent->d_type, dent->d_ino);
+    if (fork() == 0) {
+        printf("Signalling my parent now\n");
+        kill(1, 60);
+        return 0;
     }
-
-    run((char *[]){"/usr/bin/bash", NULL});
-
+    
+    char const whitespace[] = " \t\r\n";
+    const size_t buffer_len = 1024;
+    char buffer[buffer_len];
+    const size_t argbuf_len = 32;
+    char const *argbuf[argbuf_len+1];
+    while (1) {
+        readline(buffer, buffer_len);
+        size_t i = 0;
+        size_t narg = 0;
+        while (i < buffer_len && buffer[i] && narg < argbuf_len) {
+            i += strspn(buffer + i, whitespace);
+            size_t len = strcspn(buffer + i, whitespace);
+            if (len) {
+                buffer[i + len] = 0;
+                argbuf[narg++] = buffer + i;
+                i += len + 1;
+            }
+        }
+        argbuf[narg] = NULL;
+        if (narg) {
+            run(argbuf);
+        }
+    }
+    
     return 0;
 }
